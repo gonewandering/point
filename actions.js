a6f8770e78c915171571c28200148eeae025dc5f
Lindsay Silver <lgsilver@gmail.com>
2018-07-15 11:15:07 -0400

    Added some things

--- actions.js
+++ actions.js
@@ -23,9 +23,18 @@ module.exports = {
   left: () => {
     leds.set('update')
     console.log('starting update')
-    cp.exec('git pull origin master && npm install', (err, stout, sterr) => {
-      console.log(err, stout, sterr)
+    cp.exec('git pull origin master', (err, stout, sterr) => {
+      leds.set('update2')
+      console.log('1. Pulled latest version')
+      cp.exec('npm install', (err, stout, sterr) => {
+        leds.set('update3')
+        console.log('2. Installed Dependencies')
+      }
       process.exit(1)
     })
+  },
+  right: () => {
+    leds.set('blank')
+    process.exit(1)
   }
 }
