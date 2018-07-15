faed7bdb158b17c6c5b2d056f7046ec5bed0a06c
Lindsay Silver <lgsilver@gmail.com>
2018-07-15 10:49:54 -0400

    Added update functionality

--- actions.js
+++ actions.js
@@ -1,20 +1,31 @@
 const leds = require('./lib/leds')
 const Sensors = require('./lib/sensors')
 const sensors = new Sensors(500)
+const exec = require('child_process')
 
 var act = function () { }
-sensors.on(act)
 sensors.monitor()
+
+function (data) {
+  for (var n in data.accel) {
+
+  }
+}
 
 module.exports = {
   up: () => {
     sensors.track(console.log)
-    act = console.log
     leds.set('arrow')
   },
   down: () => {
     sensors.track()
     act = function () { }
     leds.set('cross')
+  },
+  left: () => {
+    leds.set('update')
+    exec('git pull origin master').then(() => {
+      process.exit()
+    })
   }
 }
--- sense.js
+++ sense.js
@@ -2,8 +2,8 @@ var request = require('request-promise-native')
 var mid = require('node-machine-id')
 const imu = require("node-sense-hat").Imu;
 const JoystickLib = require("node-sense-hat").Joystick;
-const IMU = new imu.IMU();
-const leds = require('./lib/leds')
+const IMU = new imu.IMU()
+const actions = require('./actions')
 
 var options = {
   monitoring: false
@@ -11,34 +11,29 @@ var options = {
 
 JoystickLib.getJoystick().then(joystick => {
   joystick.on("release", direction => {
-    options.monitoring = !options.monitoring
-    leds.set(options.monitoring ? 'arrow' : 'cross')
+    actions[direction]()
   })
 })
 
-
-function track() {
-  IMU.getValue((err, data) => {
-    // analyzeChange(data.accel)
-    console.log(data.accel)
-  })
-}
-
-function monitor() {
-  setTimeout(() => {
-    if (options.monitoring == true) {
-      track().then(monitor)
-    }
-    monitor()
-  }, 10 0)
-}
-
-function analyzeChange(data) {
-  for (var n in data) {
-    change[n] = change[n] || {l: 0, d: 0}
-    change[n].d = Math.abs(Math.abs(change[n].l) - Math.abs(data[n]))
-
-  }
-}
-
-monitor()
+// function track() {
+//
+// }
+//
+// function monitor() {
+//   setTimeout(() => {
+//     if (options.monitoring == true) {
+//       track().then(monitor)
+//     }
+//     monitor()
+//   }, 10 0)
+// }
+//
+// function analyzeChange(data) {
+//   for (var n in data) {
+//     change[n] = change[n] || {l: 0, d: 0}
+//     change[n].d = Math.abs(Math.abs(change[n].l) - Math.abs(data[n]))
+//
+//   }
+// }
+//
+// monitor()
