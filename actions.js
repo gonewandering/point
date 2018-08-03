3ba63bfbfefc22d9fc5a3241133162a4e0fbefdb
Lindsay Silver <lgsilver@gmail.com>
2018-08-02 21:59:18 -0400

    Fix async bug

--- actions.js
+++ actions.js
@@ -1,4 +1,4 @@
-const device = require('point-device-sdk')
+const dev = require('point-device-sdk')
 const sensors = require('./lib/sensors')
 const schemes = require('./schemes')
 const leds = require('./lib/leds')
@@ -11,46 +11,46 @@ const actions = {}
 
 actions.on = function () {
   leds.set('idle')
-  device.status.send('on')
+  dev.status.send('on')
 }
 
 actions.track = function () {
   leds.set('track')
-  device.status.send('tracking')
+  dev.status.send('tracking')
 
   sensors.on(res => {
     if (res.rp % 10 == 0) {
-      device.log.send({device: { id: device.mid }, type: 'z-angle', value: data.gyro.x }),
-      device.log.send({device: { id: device.mid }, type: 'y-angle', value: data.gyro.y }),
-      device.log.send({device: { id: device.mid }, type: 'x-angle', value: data.gyro.z }),
-      device.log.send({device: { id: device.mid }, type: 'temperature', value: data.temperature }),
-      device.log.send({device: { id: device.mid }, type: 'pressure', value: data.pressure })
+      dev.log.send({device: { id: dev.mid }, type: 'z-angle', value: data.gyro.x }),
+      dev.log.send({device: { id: dev.mid }, type: 'y-angle', value: data.gyro.y }),
+      dev.log.send({device: { id: dev.mid }, type: 'x-angle', value: data.gyro.z }),
+      dev.log.send({device: { id: dev.mid }, type: 'temperature', value: data.temperature }),
+      dev.log.send({device: { id: dev.mid }, type: 'pressure', value: data.pressure })
     }
   })
 }
 
 actions.pause = function () {
   leds.set('idle')
-  device.status.send('paused')
+  dev.status.send('paused')
   sensors.pause()
 }
 
 actions.update = async function () {
   leds.set('update1')
 
-  await device.status.send('updating')
+  await dev.status.send('updating')
   await exec('git pull origin master')
   await leds.set.bind(null, 'update2')
   await exec.bind(null, 'npm install')
   await leds.set.bind(null, 'update3')
-  await device.status.send('update-complete')
+  await dev.status.send('update-complete')
 
   return
 }
 
-actions.restart = function () {
+actions.restart = async function () {
   leds.set('idle')
-  await device.status.send('restarting')
+  await dev.status.send('restarting')
 
   process.exit(1)
 }
