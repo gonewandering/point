ee9759b410eb69849cb1c9a56c65c37008a70e0b
Lindsay Silver <lgsilver@gmail.com>
2018-08-04 15:30:35 -0400

    Updates galore...

--- actions.js
+++ actions.js
@@ -9,46 +9,46 @@ const actions = {}
 
 actions.on = function () {
   leds.set('idle')
-  dev.status.send('on')
+  dev.status.update('on')
 }
 
 actions.track = function () {
   leds.set('track')
-  dev.status.send('tracking')
+  dev.status.update('tracking')
 
   sensors.on(res => {
     if (res.rp % 10 == 0) {
-      dev.log.send({device: { id: dev.mid }, type: 'z-angle', value: data.gyro.x }),
-      dev.log.send({device: { id: dev.mid }, type: 'y-angle', value: data.gyro.y }),
-      dev.log.send({device: { id: dev.mid }, type: 'x-angle', value: data.gyro.z }),
-      dev.log.send({device: { id: dev.mid }, type: 'temperature', value: data.temperature }),
-      dev.log.send({device: { id: dev.mid }, type: 'pressure', value: data.pressure })
+      dev.log.send({timestamp: event: 'z-angle', value: data.gyro.x }),
+      dev.log.send({event: 'y-angle', value: data.gyro.y }),
+      dev.log.send({event: 'x-angle', value: data.gyro.z }),
+      dev.log.send({event: 'temperature', value: data.temperature }),
+      dev.log.send({event: 'pressure', value: data.pressure })
     }
   })
 }
 
 actions.pause = function () {
   leds.set('idle')
-  dev.status.send('paused')
+  dev.status.update('paused')
   sensors.pause()
 }
 
 actions.update = async function () {
   leds.set('update1')
 
-  await dev.status.send('updating')
+  await dev.status.update('updating')
   await exec('git pull origin master')
   await leds.set.bind(null, 'update2')
   await exec.bind(null, 'npm install')
   await leds.set.bind(null, 'update3')
-  await dev.status.send('update-complete')
+  await dev.status.update('update-complete')
 
   return
 }
 
 actions.restart = async function () {
   leds.set('idle')
-  await dev.status.send('restarting')
+  await dev.status.update('restarting')
 
   process.exit(1)
 }
