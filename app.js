0e96fc3439b6ab2b9123c76050594eb06ed7f9d2
Lindsay Silver <lgsilver@gmail.com>
2018-08-04 16:14:26 -0400

    Added on command

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const setupJoystick = async function () {
 
 const init = async function () {
   await setupJoystick()
-
+  dev.command.send('on')
+  
   dev.command.on('*', cmd => {
     actions[cmd] && actions[cmd]()
   })
