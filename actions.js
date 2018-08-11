39926345eeb2b56e905b6e6408bce64d4942853f
Lindsay Silver <lgsilver@gmail.com>
2018-08-11 07:39:15 -0400

    Added to gitignore

--- actions.js
+++ actions.js
@@ -1,6 +1,5 @@
 const dev = require('point-device-sdk')
 const sensors = require('./lib/sensors')
-const leds = require('./lib/leds')
 const exec = require('./lib/hlp/exec')
 const process = require('process')
 const config = require('./config')
@@ -8,8 +7,6 @@ const config = require('./config')
 const actions = {}
 
 actions.on = function () {
-  console.log( `Starting device: ${dev.mid}`)
-  leds.set('idle')
   dev.status.update('on')
 }
 
@@ -20,7 +17,6 @@ actions.config = async function (data) {
 }
 
 actions.track = function () {
-  leds.set('track')
   dev.status.update('tracking')
 
   let freq = Math.round((dev.config.get('freq') || 1000) / 100)
@@ -29,17 +25,12 @@ actions.track = function () {
 
   sensors.on(res => {
     if (sensors.rp % freg == 0) {
-      dev.log.send({event: 'z-angle', value: res.gyro.x }),
-      dev.log.send({event: 'y-angle', value: res.gyro.y }),
-      dev.log.send({event: 'x-angle', value: res.gyro.z }),
-      dev.log.send({event: 'temperature', value: res.temperature }),
-      dev.log.send({event: 'pressure', value: res.pressure })
+      schemes["people"]()
     }
   })
 }
 
 actions.pause = function () {
-  leds.set('idle')
   dev.status.update('paused')
   sensors.pause()
 }
@@ -49,18 +40,14 @@ actions.update = async function () {
 
   await dev.status.update('updating')
   await exec('git pull origin master')
-  await leds.set.bind(null, 'update2')
-  await exec.bind(null, 'npm install')
-  await leds.set.bind(null, 'update3')
+  await exec('npm install')
   await dev.status.update('update-complete')
 
   return
 }
 
 actions.restart = async function () {
-  leds.set('idle')
   await dev.status.update('restarting')
-
   process.exit(1)
 }
 
--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const JoystickLib = require('node-sense-hat').Joystick;
 const dev = require('point-device-sdk')
-const motions = require('./motions')
 const actions = require('./actions')
 
 const init = async function () {
-  let joystick = await JoystickLib.getJoystick()
-
-  joystick.on('release', dir => {
-    motions[dir]()
-  })
-
   actions['on']()
 
   dev.command.on('*', (cmd, data) => {
